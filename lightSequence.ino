5a390e2413030ceda10bfcba71cae3524f068bd4
FlemliFake <49492077+FlemliFake@users.noreply.github.com>
2019-04-16 14:08:21 +0200

    Add files via upload

---
+++ lightSequence.ino
@@ -0,0 +1,50 @@
+/*
+  Light sequence
+
+  Turns an LED on for the set time by the potentiometer. After the specific period, it turns the current one off
+  and jumps to the next one till the chain is finished. This cyclus will be repeated infinitly.
+
+  Created: 09 April 2019
+  Autoher: Patrick Scheidegger
+
+  This code was intentionally created for the LB 2 of the learning unit (module) 242.
+*/
+ 
+// the ID's of the pins
+const int PINS[] = {10,9,8,7,6,5,4,3,2};
+
+const int POT_ID = 0;
+
+void setup() {
+  Serial.begin(9600);
+  // initialize the LEDs and turn them off
+  for(int i = 0; i < 9; i++){
+    pinMode(PINS[i],OUTPUT);
+    digitalWrite(PINS[i],LOW);
+  }
+}
+
+/*
+ * Gets the time out based on the user input
+ * of the potentiometer
+ * 
+ * return int, time out duration in milliseconds
+ */
+int getTimeOut(){
+  int timeOut = analogRead(POT_ID);
+  // set minimum time out
+  if(timeOut <= 40){
+    timeOut = 40;  
+  }
+  timeOut /= 2;
+  return timeOut;
+}
+
+void loop() {
+  // turn one LED on for a specific time, then turn it off
+  for(int i = 0; i < 9; i++){
+    digitalWrite(PINS[i],HIGH);
+    delay(getTimeOut());
+    digitalWrite(PINS[i],LOW);
+  }
+}
---
+++ pressSpeedGame.ino
@@ -0,0 +1,48 @@
+/*
+  Press speed game
+
+  The player has to press the button as quick as possible. The more clicks he did the more LEDs
+  will get turned on. The game will restart if all LEDs are turned on and some additional clicks are done.
+
+  Created: 09 April 2019
+  Autoher: Patrick Scheidegger
+
+  This code was intentionally created for the LB 2 of the learning unit (module) 242.
+*/
+
+// the number of the pushbutton pin
+const int BUTTON = 2;
+// the ID's of the pins
+const int PINS[] = {3,4,5,6,7};
+// amount of points which are needed to reach the next state
+const int DISTANCE = 200;
+// current score of the player
+int playerScore = 0;
+
+void setup() {
+  Serial.begin(9600);
+  // initialize the LEDs and turn them off
+  for(int i = 0; i < 5; i++){
+    pinMode(PINS[i],OUTPUT);
+    digitalWrite(PINS[i],LOW);
+  }
+  // initialize the pushbutton
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value
+  int buttonState = digitalRead(BUTTON);
+  if (buttonState == HIGH) {
+    playerScore++;
+  }
+  digitalWrite(PINS[playerScore / DISTANCE],HIGH);
+  // if all LEDs are turned on and enough clicks were done
+  // reset the game
+  if(playerScore > (5 * DISTANCE)){
+    playerScore = 0;
+    for(int i = 0; i < 5; i++){
+      digitalWrite(PINS[i],LOW);
+    }
+  }
+}
